1a7722913e17bc517cbba2603577e06a865ce55c
Juan <juanc.herrera@cetys.edu.mx>
2023-05-08 11:49:18 -0700

    Added styles to the buttons

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const clearbutton = document.querySelector(".clear");
 const changeSize = document.querySelector(".changeButton");
 const ColorButton = document.querySelector(".changeColor");
 const colorWindow = document.querySelector(".colorWindow");
+const rainbowButton = document.querySelector(".rainbow");
 
 let color = "#000000";
 let bufferColor = "";
 let mouseClick = false;
+let rainbow = false;
+let eraserOn = false;
+
 
 document.body.onmousedown = () => {mouseClick = true}
 document.body.onmouseup = () => {mouseClick = false}
@@ -19,13 +23,38 @@ function start(){
    ColorButton.addEventListener('click',function(e){
       selectColor();
    })
+   rainbowButton.addEventListener('click',function(e){
+      if(rainbow == true){
+         rainbow = false;
+         color = bufferColor;
+         rainbowButton.classList.remove("focus");
+      }
+      else{
+         rainbow = true;
+         bufferColor = color;
+         rainbowButton.classList.add("focus");
+
+      }
+   })
 
    changeSize.addEventListener('click',function(e){
       changeGrid();
    })
 
    eraser.addEventListener('click',function(e){
-      erase();
+     
+      if(eraserOn == true){
+         color = bufferColor;
+         eraser.classList.remove("focus");
+         eraserOn = false;
+      }
+      else{
+         eraserOn = true;
+         bufferColor = color;
+         erase();
+         eraser.classList.add("focus");
+
+      }
    })
 
    clearbutton.addEventListener('click',function(e){
@@ -39,10 +68,14 @@ function selectColor(){
    bufferColor = color;
 }
 function changeColor(e){  
+   console.log(rainbow);
    if(e.type === "mouseover" && !mouseClick){return}
-   e.target.style.backgroundColor = color;
+   if(rainbow == true){
+      color = RGB2HTML(getRandomNumber(0,256),getRandomNumber(0,256),getRandomNumber(0,256));
+   }
+      e.target.style.backgroundColor = color;
+   
 }
-
 function createGrid(pixels){
    for(let i = 0; i < pixels; i++){
       let newDiv = document.createElement('div');
@@ -73,14 +106,15 @@ function changeGrid(){
 }
 
 function erase(){
-
-   if(color !== '#ffffff'){
-      bufferColor = color;
-      color = '#ffffff';
-   }
-   else{
-      color = bufferColor;
-   }
+   eraserOn = true;
+   color = '#ffffff';
+   // if(color !== '#ffffff'){
+   //    bufferColor = color;
+   //    color = '#ffffff';
+   // }
+   // else{
+   //    color = bufferColor;
+   // }
 }
 function clear(){
    const divs = document.querySelectorAll(".pixel");
@@ -88,5 +122,14 @@ function clear(){
       div.style.backgroundColor = "#ffffff";
       color = bufferColor;
    });
+}
+
+const getRandomNumber  = (min,max)=>{
+   number = Math.floor(Math.random() * (max - min) + min);
+   return number;
+} 
+const RGB2HTML = (red,green,blue) =>{
+   var decColor =0x1000000+ blue + 0x100 * green + 0x10000 *red ;
+   return '#'+decColor.toString(16).substr(1);
 }
 
