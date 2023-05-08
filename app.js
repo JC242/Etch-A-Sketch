b517c906a2f8862e4dd4802d068e6cf7f3a7572b
Juan <juanc.herrera@cetys.edu.mx>
2023-05-08 08:03:04 -0700

    Added hover effect

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const container = document.querySelector(".container");
 
 
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < 256; i++){
    let newDiv = document.createElement('div');
-   newDiv.id = 'r'+i;
-   newDiv.textContent="hello";
+   newDiv.id = 'pixel'+i;
+   newDiv.addEventListener("mouseover",function(e){
+      newDiv.style.backgroundColor = "red";
+   })
+   
    container.appendChild(newDiv);
 }
-
