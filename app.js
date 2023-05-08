bdc4d79d92c91fa3b007838a30cd4e53aa48285e
Juan <juanc.herrera@cetys.edu.mx>
2023-05-08 09:40:26 -0700

    Added change size button

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const grid = document.querySelector(".grid");
 const eraser = document.querySelector(".eraser");
 const clearbutton = document.querySelector(".clear");
+const changeSize = document.querySelector(".changeButton");
 
 let color = "black";
 
+start();
 
-function createGrid(){
-   for(let i = 0; i < 256; i++){
+function start(){
+   changeSize.addEventListener('click',function(e){
+      changeGrid();
+   })
+
+   eraser.addEventListener('click',function(e){
+      erase();
+   })
+
+   clearbutton.addEventListener('click',function(e){
+      clear();
+   })
+   createGrid(256);
+}
+
+function createGrid(pixels){
+   for(let i = 0; i < pixels; i++){
       let newDiv = document.createElement('div');
       newDiv.id = 'pixel'+i;
       newDiv.classList.add("pixel");
@@ -17,15 +34,25 @@ function createGrid(){
    }
 }
 
-function start(){
-   eraser.addEventListener('click',function(e){
-      erase();
-   })
+function changeGrid(){
 
-   clearbutton.addEventListener('click',function(e){
-      clear();
-   })
-   createGrid();
+   let colums = prompt("Please enter number columns, Max 100", 1);
+   while(colums > 100){
+      colums = prompt("Please enter number columns, Max 100" , 1);
+   }
+   let rows = prompt("Please enter number of rows, Max 100",1);
+   while(rows > 100){
+      rows = prompt("Please enter number of rows, Max 100",1);
+   }
+   let numberOfpixels = colums * rows;
+   grid.style.gridTemplateColumns = `repeat(${colums},1fr)`;
+   grid.style.gridTemplateRows = `repeat(${rows},1fr)`;
+
+   const pixels = document.querySelectorAll(".pixel");
+
+   pixels.forEach((e) => e.parentNode.removeChild(e));
+   createGrid(numberOfpixels);
+   color = 'black';
 }
 
 function erase(){
@@ -44,4 +71,3 @@ function clear(){
    });
 }
 
-start();
